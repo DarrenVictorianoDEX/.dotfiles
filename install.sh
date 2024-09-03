d9a7c3c3a0b298cc531fe5d113e4af980b74d887
Darren Victoriano <darren.victoriano@gmail.com>
2024-09-03 15:17:50 -0700

    updated oh-my-zsh installtion

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ install_stow() {
 install_oh_my_zsh() {
   if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Installing oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     echo "oh-my-zsh is installed."
   else
     echo "oh-my-zsh is already installed."
