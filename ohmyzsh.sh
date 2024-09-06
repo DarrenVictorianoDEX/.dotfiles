77a481b15f04fe514fb61e43aa962ab08e77818a
Darren Victoriano <darren.victoriano@gmail.com>
2024-09-05 23:13:57 -0700

    fix bug

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -12,43 +12,22 @@ install_oh_my_zsh() {
   fi
 }
 
-install_powerlevel10k() {
-  local p10k_dir="${ZSH_CUSTOM}/themes/powerlevel10k"
-  if [ ! -d "$p10k_dir" ]; then
-    echo "Installing powerlevel10k..."
-    mkdir -p "${ZSH_CUSTOM}/themes"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$p10k_dir"
-    echo "powerlevel10k is installed."
+install_plugin() {
+  local git_clone_command=$1
+  # Check if the ~/.oh-my-zsh directory exists
+  if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing: $git_clone_command"
+    eval "$git_clone_command"
+    echo "Plugin/theme installed."
   else
-    echo "powerlevel10k is already installed."
-  fi
-}
-
-install_autosuggestions() {
-  local autosuggest_dir="${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
-  if [ ! -d "$autosuggest_dir" ]; then
-    echo "Installing zsh-autosuggestions..."
-    mkdir -p "${ZSH_CUSTOM}/plugins"
-    git clone https://github.com/zsh-users/zsh-autosuggestions "$autosuggest_dir"
-    echo "zsh-autosuggestions installed."
-  else
-    echo "zsh-autosuggestions is already installed."
-  fi
-}
-
-install_syntax_highlighting() {
-  local syntax_highlight_dir="${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
-  if [ ! -d "$syntax_highlight_dir" ]; then
-    echo "Installing zsh-syntax-highlighting..."
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$syntax_highlight_dir"
-    echo "zsh-syntax-highlighting installed."
-  else
-    echo "zsh-syntax-highlighting is already installed."
+    echo "~/.oh-my-zsh directory does not exist. Cannot install powerlevel10k."
   fi
 }
 
 # Execute functions
 install_oh_my_zsh
-install_powerlevel10k
-install_autosuggestions
-install_syntax_highlighting
+
+# Install Plugins
+install_plugin "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+install_plugin "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+install_plugin "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
