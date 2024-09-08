aa9d5a57e33b44e4e544ffa7e176fbb6550c97a2
Darren Victoriano <darren.victoriano@gmail.com>
2024-09-08 16:22:32 -0700

    refactor scripts

--- install.sh
+++ install.sh
@@ -18,21 +18,22 @@ install_ohmyzsh() {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/DarrenVictoriano/.dotfiles/main/_scripts/ohmyzsh.sh)"
 }
 
+clone_dotfiles_and_stow() {
+  echo "Start Stowing process using _scripts/stow.sh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/DarrenVictoriano/.dotfiles/main/_scripts/stow.sh)"
+}
+
 update_app_settings() {
   echo "changing MacOS defaults using _scripts/app_settings.sh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/DarrenVictoriano/.dotfiles/main/_scripts/app_settings.sh)"
 }
 
-
-
-
-
 # Main function to run all installations and configurations
 install_brew_packages
 install_from_appstore
 install_ohmyzsh
-
+clone_dotfiles_and_stow
 update_app_settings
 
 # Execute main function
-echo "\nSetup completed. Quit this terminal session (cmd + q) then relaunch iTerm2 to see the updates."
+echo "\nSetup completed. Quit this terminal session (cmd + q) then launch iTerm2 to see the updates."
