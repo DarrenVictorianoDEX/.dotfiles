36a0096bfc6d26d6c6bcf2a096adc3144f6b5046
Darren Victoriano <darren.victoriano@gmail.com>
2024-10-09 22:16:47 -0700

    python lsp update

--- nvim/.config/nvim/lua/darren/plugins/linting.lua
+++ nvim/.config/nvim/lua/darren/plugins/linting.lua
@@ -10,7 +10,7 @@ return {
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			-- svelte = { "eslint_d" },
-			python = { "mypy", "pylint" },
+			python = { "pylint" },
 			-- TODO: add other lint
 			-- robot
 			-- clang
